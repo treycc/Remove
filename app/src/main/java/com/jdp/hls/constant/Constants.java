d8da3ab95c33a573e3af42af73dffd667a77cc75
KingJA <kingjavip@gmail.com>
2019-03-06 09:31:00 +0800

    省市区优化

--- app/src/main/java/com/jdp/hls/constant/Constants.java
+++ app/src/main/java/com/jdp/hls/constant/Constants.java
@@ -10,9 +10,9 @@ import com.jdp.hls.util.EncryptUtil;
  * 修改备注：
  */
 public class Constants {
-        public static final String BASE_URL = "http://192.168.0.2:8080/";//本地
+//        public static final String BASE_URL = "http://192.168.0.2:8080/";//本地
 //    public static String BASE_URL = "http://192.168.0.5:8081/";//服务器
-//    public static final String BASE_URL = "http://api.fwzspt.cn/";//外网
+    public static final String BASE_URL = "http://api.fwzspt.cn/";//外网
 
     public static final int PAGE_SIZE = 20;
     public static final int PAGE_FIRST = 1;
